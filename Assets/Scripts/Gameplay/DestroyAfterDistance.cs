582014509197a8ac63d6925e8f12fb3d9ba85436
Valentina Preciado Nuñez <valisprenun2401052019@gmail.com>
2026-08-03 21:50:40 -0500

    Fix GameManager an DestroyAfterDistance

--- Assets/Scripts/Gameplay/DestroyAfterDistance.cs
+++ Assets/Scripts/Gameplay/DestroyAfterDistance.cs
@@ -1,6 +1,5 @@
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.tvOS;
 
 public class DestroyAfterDistance : MonoBehaviour
 {
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using static UnityEngine.InputManagerEntry;
 
 public class GameManager : MonoBehaviour
 {
